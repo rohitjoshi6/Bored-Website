3d2b62cdb28f94ecf9d1ca875417a14d543d2812
Rohit_Joshi <rohitjoshi632001@gmail.com>
2021-07-13 18:45:36 +0530

    Added function for activity

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-fetch('http://www.boredapi.com/api/activity/')
+/*fetch('http://www.boredapi.com/api/activity/')
 .then(Response=> Response.json())
-.then(Data=>console.log(Data.activity))
+.then(Data=>console.log(Data.activity))*/
+
+
+const textDisplay=document.querySelector("#text-area");
+const button=document.querySelector('#glass-btn');
+
+button.addEventListener('click', async function(){
+
+    const res= await fetch('http://www.boredapi.com/api/activity/');
+    const myJson= await res.json();
+
+    let myString= JSON.stringify(myJson);
+
+    let myobject=JSON.parse(myString);
+
+    textDisplay.textContent= myobject.activity;
+
+})
